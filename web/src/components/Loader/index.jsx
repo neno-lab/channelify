ef01758c8bec876b90f29d91c9430d30949c1bbf
neno.zidic <neno.zidic@fuel-gaming.com>
2021-05-27 09:37:45 +0200

    Small fix

--- web/src/components/Loader/index.jsx
+++ web/src/components/Loader/index.jsx
@@ -3,8 +3,8 @@ import './style.scss';
 
 const Loader = () => {
   return (
-    <div class='box'>
-      <div class='loader-34'></div>
+    <div className='box'>
+      <div className='loader-34'></div>
     </div>
   );
 };
