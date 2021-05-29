75f23c51de33c6566ebe80852898bd8ac1f50b48
neno.zidic <neno.zidic@fuel-gaming.com>
2021-05-29 20:46:48 +0200

    Ready for deploy

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ webpush.setVapidDetails(
 
 app.use('/api/v1', router);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'web/build/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
