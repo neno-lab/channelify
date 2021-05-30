391feb5861e615d454bde980e6ecbe5790be42f8
neno.zidic <neno.zidic@fuel-gaming.com>
2021-05-30 14:28:20 +0200

    ssl

--- db/index.js
+++ db/index.js
@@ -7,9 +7,10 @@ const prodConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString:
-    process.env.NODE_ENV === 'production'
-      ? 'postgres://ocubhwxxnfyvnb:94d03711812a76f7fc6bca29a2a745f8f165b18aef36b15a6be7666d9d346e6a@ec2-3-233-7-12.compute-1.amazonaws.com:5432/da9ha84r8kg35p'
-      : devConfig,
+    process.env.NODE_ENV === 'production' ? prodConfig : devConfig,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
